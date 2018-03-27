35e29564e068fcf0a4a1ca856aab0c80db54d848
Kirk Franklin <kirk@magneato.com>
2018-03-27 12:24:03 -0700

    Initial testing setup

--- tests/GuzzleGoferFunctionTest.php
+++ tests/GuzzleGoferFunctionTest.php
@@ -1,9 +1,31 @@
 <?php declare(strict_types=1);
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Exception\RequestException;
 use Kirkaracha\GuzzleGofer\GuzzleGofer;
 
 class GuzzleGoferFunctionTest
 {
+    /** @var $mock */
+    protected $mock;
+
+    public function __construct()
+    {
+        $this->mock = new MockHandler([
+            new Response(200, ['X-Foo' => 'Bar']),
+            new Response(202, ['Content-Length' => 0]),
+            new RequestException("Error Communicating with Server",
+            new Request('GET', 'test'))
+        ]);
+
+        $handler = HandlerStack::create($this->mock);
+        $client = new Client(['handler' => $handler]);
+    }
+
     /**
      * Check that the multiply method returns correct result
      * @return void
